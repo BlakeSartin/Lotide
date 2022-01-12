b9a739c80b69052434153a3471d3e1e6c52aaec7
Blake Sartin <blake.sartin8@gmail.com>
2022-01-12 23:28:10 +0000

    Finds matching key

---
+++ findKeyByValue.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+}
+
+const findKeyByValue = function(object, value) {
+  const array = Object.keys(object);
+  for (let element of array) {
+    if (object[element] === value) return element;
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
