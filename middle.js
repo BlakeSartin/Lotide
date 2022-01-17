bc3ed746a64810d255199e09e50cb9e2d4139e2b
Blake Sartin <blake.sartin8@gmail.com>
2022-01-17 19:47:04 +0000

    middle refact

--- middle.js
+++ middle.js
@@ -1,23 +1,18 @@
-const eqArrays = (actual, expected) =>
-actual.length === expected.length &&
-actual.every((v, i) => v === expected[i]);
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
-const middle = function(array) {
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+const middle = function (array) {
   if (array.length <= 2) {
     return [];
   } else if (array.length % 2 === 0) {
-    return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+    return [
+      array[Math.ceil(array.length / 2) - 1],
+      array[Math.ceil(array.length / 2)],
+    ];
   } else {
     return [array[Math.floor(array.length / 2)]];
   }
 };
 
-console.log(assertArraysEqual(middle([1, 2])))
+module.exports = middle;
+
+
