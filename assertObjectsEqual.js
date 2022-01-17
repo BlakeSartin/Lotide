9f7e6e5db2651f0813d13b1c95790f694ae6c072
Blake Sartin <blake.sartin8@gmail.com>
2022-01-17 23:11:30 +0000

    assert Object equal refact

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,18 +1,4 @@
-const eqObjects = function(object1, object2) {
-  array = ""
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false
-  }
-  for (let key of Object.keys(object1)) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      array = eqArrays(object1[key], object2[key])
-      return array
-    } else if (object1[key] !== object2[key]) {
-      return false
-    }
-  }
-  return true
-}
+const eqObjects = require('./eqObjects')
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -22,4 +8,5 @@ const assertObjectsEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 }
-console.log(eqObjects({ a: '1', b: '3' }, { a: '1', b: 2 }))
+
+module.exports = assertObjectsEqual
