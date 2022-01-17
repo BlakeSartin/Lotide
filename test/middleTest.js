6acef60a6d5677c13fbe86b9043296c27f9691d4
Blake Sartin <blake.sartin8@gmail.com>
2022-01-17 21:44:46 +0000

    middle test refact

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,2 +1,11 @@
 const middle = require('../middle')
-const assertArraysEqual = require('../assertArraysEqual')
+const assert= require('chai').assert
+
+describe ("#middle", () => {
+  it ("return 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  })
+  it ("return 2, 3 for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+  })
+})
