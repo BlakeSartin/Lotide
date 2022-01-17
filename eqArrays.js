9b0a3062a46bec569e2fbc80e31d646b435d7d03
Blake Sartin <blake.sartin8@gmail.com>
2022-01-17 19:36:45 +0000

    refact eqArray

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,7 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-}
+const assertEqual = require('./assertEqual')
 
 const eqArrays = (actual, expected) =>
 actual.length === expected.length &&
 actual.every((v, i) => v === expected[i]);
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays
