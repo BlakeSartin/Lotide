c72f11234030214c3e9ba6ef6778624b91a6d1ad
Blake Sartin <blake.sartin8@gmail.com>
2022-01-17 21:46:50 +0000

    tail test refact

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,13 @@
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert
 const tail = require('../tail')
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+//tail(words); // no need to capture the return value since we are not checking it
+//assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe ("#tail", () => {
+  it ("return 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3)
+  })
+})
