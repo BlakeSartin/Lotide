5e6ec677c378ea64b69caaa2fafaed996bb50e50
Blake Sartin <blake.sartin8@gmail.com>
2022-01-17 21:42:49 +0000

    middle refact

--- middle.js
+++ middle.js
@@ -1,16 +1,19 @@
 const eqArrays = require("./eqArrays");
 const assertArraysEqual = require("./assertArraysEqual");
 const middle = function (array) {
+  console.log(typeof(array))
+  var result = []
   if (array.length <= 2) {
-    return [];
+    return result;
   } else if (array.length % 2 === 0) {
-    return [
-      array[Math.ceil(array.length / 2) - 1],
-      array[Math.ceil(array.length / 2)],
-    ];
+    console.log("even")
+    result.push(array[Math.ceil(array.length / 2) - 1]) && result.push(array[Math.ceil(array.length / 2)])
   } else {
-    return [array[Math.floor(array.length / 2)]];
+    console.log("odd")
+    result.push(array[Math.floor(array.length / 2)]);
   }
+  console.log(typeof(result), "result")
+  return result
 };
 
 module.exports = middle;
