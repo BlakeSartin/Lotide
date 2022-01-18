7d65175d56727b2e2598b2fd2dd44984c5cf2edf
Blake Sartin <blake.sartin8@gmail.com>
2022-01-18 00:37:08 +0000

    test for count only

--- test/countOnlytest.js
+++ test/countOnlytest.js
@@ -1,12 +1,25 @@
 
 const assert = require('chai').assert
 const countOnly = require('../countOnly')
-
 describe ("#countOnly", () => {
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ]
   it ("return Jason for Jason", () => {
-    assert.strictEqual(countOnly(["Jason"]), 1)
+    const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false })
+
+    assert.strictEqual((result1["Jason"]), 1)
   })
 })
+
 
 
 
