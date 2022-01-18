0b6335bc51f1a5da4b9cfa41ed5c1a98e65331b6
Blake Sartin <blake.sartin8@gmail.com>
2022-01-18 00:55:02 +0000

    index

---
+++ index.js
@@ -0,0 +1,49 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const eqObjects = require("./eqObjects")
+const assertObjectsEqual = require("./assertObjectsEqual")
+const countLetters = require("./countLetters");
+const findKey = require("./findKey");
+const flatten = require("./flatten");
+const letterPositions = require("./letterPositions");
+const findKeyByValue = require("./findKeyByValue");
+const map = require("./map");
+const countOnly = require("./countOnly");
+const without = require("./without");
+const eqArrays = require("./eqArrays");
+eqArrays = require('./eqArrays')
+findKey = require('./findKey')
+flatten = require('./flatten')
+letterPositions = require('./letterPositions')
+findKeyByValue = require('./findKeyByValue')
+map = require('./map')
+countOnly = require('./countOnly')
+without = require('./without')
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqObjects: eqObjects,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  eqArrays: eqArrays,
+  findKey: findKey,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  findKeyByValue: findKeyByValue,
+  map: map,
+  countOnly: countOnly,
+  without: without
+  
+
+
+
+
+
+};
