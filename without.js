251408a2b845eabac60bba826dd855f910813fc7
Blake Sartin <blake.sartin8@gmail.com>
2022-01-18 00:54:03 +0000

    without function refact

--- without.js
+++ without.js
@@ -1,14 +1,6 @@
-const eqArrays = (actual, expected) =>
-actual.length === expected.length &&
-actual.every((v, i) => v === expected[i])
+const eqArrays = require('./eqArrays')
   
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual')
 
 const without = function (source, itemsToRemove) {
 let newArray = []
@@ -17,10 +9,8 @@ for (const element of source) {
     newArray.push(element)
   }
 }
-return console.log(newArray)
+return newArray
 }
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+module.exports = without
